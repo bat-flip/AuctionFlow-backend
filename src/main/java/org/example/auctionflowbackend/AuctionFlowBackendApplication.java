cd37017c1b6ba73c138e33a873dce49217254886
hyunji <phj9573@naver.com>
2024-07-25 00:29:21 +0900

    first commit

---
+++ src/main/java/org/example/auctionflowbackend/AuctionFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.auctionflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionFlowBackendApplication.class, args);
+    }
+
+}
