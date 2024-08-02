6f6afc82e54b0ebc34745677b7dbd55bc18a5acf
hyunji <phj9573@naver.com>
2024-08-02 23:32:19 +0900

    Deploy : 우분투 터미널 log 조건 변경 후 재배포

--- src/main/java/org/example/auctionflowbackend/Controller/HomeController.java
+++ src/main/java/org/example/auctionflowbackend/Controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "Auction Flow7";
+        return "Auction Flow8";
     }
 
 }
